0dd24c0a22c1b24834f420515883db4e5e2c1e3d
Emmanuel Nyakoe <emmanuelnyakoe13@gmail.com>
2025-05-12 21:52:25 +0300

    star rating

---
+++ my-app/src/components/image-slider/index.jsx
@@ -0,0 +1,8 @@
+export default function ImageSlider({url, limit}) {
+
+    const [images, setImages] = useState([])
+    const [currentSlide, setCurrentSlide] = useState(0)
+    
+  return <div className="container"></div>
+
+}
