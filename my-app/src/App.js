bf4109b28880c210a3861e7ec9a244d4ea4d6dc9
Emmanuel Nyakoe <emmanuelnyakoe13@gmail.com>
2025-05-14 23:14:01 +0300

    image slider

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -3,16 +3,19 @@ import './App.css';
 import Accordian from './components/accordian';
 import RandomColor from './components/random-color';
 import StarRating from './components/star-rating';
+import ImageSlider from './components/image-slider';
 
 function App() {
   return (
-   <div className='App'>
-    <Accordian/>
+    <div className='App'>
+      {/* <Accordian />
 
-    <RandomColor/>
+      <RandomColor />
 
-    <StarRating noOfStars={10}/>
-   </div>
+      <StarRating noOfStars={10} /> */}
+
+      <ImageSlider url={'https://picsum.photos/v2/list'} page={'1'} limit={'10'} />
+    </div>
   );
 }
 
--- my-app/src/components/image-slider/index.jsx
+++ my-app/src/components/image-slider/index.jsx
@@ -1,8 +1,74 @@
-export default function ImageSlider({url, limit}) {
+import { useEffect, useState } from "react"
+import { BsArrowLeftCircleFill, BsArrowRightCircleFill, BsArrowUpRightCircleFill } from 'react-icons/bs'
+
+export default function ImageSlider({ url, limit = 5, page = 1 }) {
 
     const [images, setImages] = useState([])
     const [currentSlide, setCurrentSlide] = useState(0)
-    
-  return <div className="container"></div>
+    const [errorMsg, setErrorMsg] = useState(null);
+    const [loading, setLoading] = useState(false);
+
+    async function fetchImages(getUrl) {
+        try {
+            setLoading(true);
+
+            const response = await fetch(
+                `${getUrl}?page=${page}&limit=${limit}`
+            );
+            const data = await response.json();
+
+            if (data) {
+                setImages(data);
+                setLoading(false);
+            }
+
+
+        } catch (e) {
+            setErrorMsg(e.message);
+            setLoading(false);
+        }
+    }
+
+    useEffect(() => {
+        if (url !== '') {
+            fetchImages(url)
+        }
+    }, [url]);
+
+    console.log(images);
+
+    if (loading) {
+        return <div>Loading...</div>
+    }
+
+    if (errorMsg !== null) {
+        return <div>Error Occurred! {errorMsg}</div>
+    }
+
+
+    return <div className="container">
+        <BsArrowLeftCircleFill className="arrow arrow-left" />
+        {images && images.length > 0 ?
+            images.map(imageItem => (
+                <img
+                    key={imageItem.id}
+                    alt={imageItem.download_url}
+                    src={imageItem.download_url}
+                    className="current-image"
+                />
+            ))
+            : null}
+            <BsArrowRightCircleFill className="arrow arrow-right" />
+            <span className="circle-indicators">
+                {
+                    images && images.length > 0?
+                    images.map{(index) => <button
+                        key={index}
+                        >
+                        </button> }
+                    :null
+                }
+            </span>
+    </div>
 
 }
