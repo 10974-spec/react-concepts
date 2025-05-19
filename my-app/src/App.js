f192fa7f28fb82caadaeb3d5a88cf43916a4b430
Emmanuel Nyakoe <emmanuelnyakoe13@gmail.com>
2025-05-19 13:37:45 +0300

    Qr code generator

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -7,6 +7,7 @@ import ImageSlider from './components/image-slider';
 import LoadMoreData from './components/load-more-data';
 import TreeView from './components/tree-view';
 import menus from "./components/tree-view/data";
+import QrCodeGenerator from './components/qr-code-generator';
 
 function App() {
   return (
@@ -19,7 +20,8 @@ function App() {
 
       {/* <ImageSlider url={'https://picsum.photos/v2/list'} page={'1'} limit={'10'} /> */}
       {/* <LoadMoreData/> */}
-      <TreeView menus={menus} />
+      {/* <TreeView menus={menus} /> */}
+      <QrCodeGenerator/>
     </div>
   );
 }
